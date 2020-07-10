8d00bcb107a971223c0c53440e11a623377f7c0b
zzyork <yorkzz1qz@163.com>
2020-07-10 14:36:04 +0800

    添加注释

--- clean_old_build_stage.sh
+++ clean_old_build_stage.sh
@@ -7,20 +7,21 @@ function clean_old_build_stage(){
     # echo "所有项目：${ALL_APP[@]}"
     for i in ${ALL_APP[@]};do
         # echo -e "当前项目：${i%?}"
-        appnum=`ls ${WORKDIR} | grep -Eo ${i}\([0-9].*?\) | wc -l`
-        time=`ls -tl ${WORKDIR} | grep -E ${i}\([0-9].*?\) | awk '{print $8}' | uniq | wc -l`
-        echo -e "\n不同创建时间的项目数：${appnum}\n"
-        if [ ${appnum} -gt 5 ]; then
-            if [ ${time} -gt 3 ]; then
-            del_appnum=`expr ${appnum} - 5`
+        appnum=`ls ${WORKDIR} | grep -Eo ${i}\([0-9].*?\) | wc -l` #取出当前项目包含的构建数量
+        time=`ls -tl ${WORKDIR} | grep -E ${i}\([0-9].*?\) | awk '{print $8}' | uniq | wc -l` #取出当前项目中代码更改的构建数量
+        echo -e "\n当前项目构建数：${appnum}\n"
+        echo -e "\n不同创建时间的构建数：${time}\n"
+        if [ ${appnum} -gt 5 ]; then #只对构建数超过5个的项目进行操作
+            if [ ${time} -gt 3 ]; then #只对代码更改超过3次的项目进行操作
+            del_appnum=`expr ${appnum} - 5` #判断需要删除的构建数量
             # echo -e "待删除数量：${del_appnum}\n"
-            del_applist=`ls -tr ${WORKDIR} | grep -Eo ${i}\([0-9].*?\) | head -${del_appnum}`
+            del_applist=`ls -tr ${WORKDIR} | grep -Eo ${i}\([0-9].*?\) | head -${del_appnum}` #列出需要删除的构建的列表
             # echo -e "`date '+%d/%b/%Y %H:%M:%S'` "
             echo -e "`date '+%d/%b/%Y %H:%M:%S'` 需要清理如下构建项目:\n${del_applist}"
             echo -e "\n`date '+%d/%b/%Y %H:%M:%S'` "
                 for list in ${del_applist}; do
                     echo -e "正在清理的的构建 ${list}..."
-                    # cd ${WORKDIR} && rm -rf ${list}
+                    cd ${WORKDIR} && rm -rf ${list}
                 done
             fi
         fi
@@ -29,12 +30,12 @@ function clean_old_build_stage(){
 }
 
 main(){
-    if [ -d ${WORKDIR} ];then
+    if [ -d ${WORKDIR} ];then #判断目录是否为空，为空则清理
         empty_dir=`find ${WORKDIR} -mindepth 1 -maxdepth 1 -type d -empty`
         # echo -e "\n空文件夹列表：$empty_dir"
         for i in ${empty_dir}; do
             echo -e "正在删除空目录${i}..."
-            # rm -r ${i}
+            rm -r ${i}
         done
         clean_old_build_stage
     fi
